5384929e4a740087ff0dfa0a559d4134c8dbcb1c
Julio Cesar <juliocesargualberto617@gmail.com>
2024-09-19 17:13:33 -0300

    add: update movie

--- Repositories/Interface/IAdminRepository.cs
+++ Repositories/Interface/IAdminRepository.cs
@@ -7,6 +7,7 @@ namespace CineApi.Repositories.Interface
     {
         Task<Guid> CreateMovieByAdmin(MovieDto movieDto, Guid userId,int quantity);
         Task<Guid> AssociateMovieByAdmin(UserMovies userMovies);
+        Task<Movie> UpdateMovieAdmin(MovieDto movieDto, Guid UserId, Guid MovieId);
 
     }
 }
